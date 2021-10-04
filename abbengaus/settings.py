e48fc78a4e50d70fa8c6e616c2fcbc35520a7dc4
Andrey Feklichev <feklichev@mail.ru>
2021-10-05 01:08:08 +0500

    Deploy version

--- abbengaus/settings.py
+++ abbengaus/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,5 @@ LOGIN_REDIRECT_URL = '/teacher/'
 LOGOUT_REDIRECT_URL = '/teacher/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn abbengaus.wsgi --log-file - 
