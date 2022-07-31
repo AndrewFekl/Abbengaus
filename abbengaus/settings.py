c7dae193e98e08e0fc9289dc0aa37c04058f5b44
Andrey Feklichev <feklichev@mail.ru>
2022-07-31 10:52:04 +0500

    Database chenged on MySql

--- abbengaus/settings.py
+++ abbengaus/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'abbengaus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'abbengaus',
+        'USER': 'first_user',
+        'PASSWORD': 'small_hacker',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
